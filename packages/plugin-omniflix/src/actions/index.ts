81b56f160a4982a1638aa416eb1fa12c857ee90c
yanamalaNaveen <naveen@layerzerox.com>
2025-02-03 17:49:24 +0530

    included the ONFT module in the plugin-omniflix

--- packages/plugin-omniflix/src/actions/index.ts
+++ packages/plugin-omniflix/src/actions/index.ts
@@ -1,7 +1,8 @@
 import { bankActions } from "./bank";
 import { stakingActions } from "./staking";
 import { govActions } from "./gov";
+import { onftActions } from "./omniflix/onft";
 
-export const actions = [...bankActions, ...stakingActions, ...govActions];
+export const actions = [...bankActions, ...stakingActions, ...govActions, ...onftActions];
 
 export default actions;
